49f1365443882b402e75aac4e4465e802964f328
Dan Brown <ssddanbrown@googlemail.com>
2021-11-29 13:10:35 +0000

    Added nicer formatting and user output

--- init.sh
+++ init.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+# Formatting constants
+RED='\033[1;31m'
+BLUE='\033[1;34m'
+GREEN='\033[1;32m'
+NC='\033[0m' # No Color
+
 # Clone down docs repo if our expected volume is empty
 if ! [ "$(ls -A /docs)" ]; then
+    echo -e "${BLUE}No docs found, Cloning down latest documentation from GitHub${NC}"
     git clone https://github.com/php/doc-en.git /docs
     chmod -R 777 /docs
 fi
@@ -11,17 +18,35 @@ ln -s /docs /phpdoc/en
 
 # Build docs
 docs_configure() {
-    php /phpdoc/doc-base/configure.php
+    echo -e "${GREEN}Documentation configuring${NC}"
+    errmsg=`php /phpdoc/doc-base/configure.php 2>&1 > /dev/null`
+    if [ ! -z "$errmsg" ]; then
+        echo -e "${RED}Configuring failed. Error:${NC}"
+        echo -e "$errmsg"
+        return 1
+    else
+        echo -e "${GREEN}Configuring complete ${NC}"
+        return 0
+    fi
 }
 docs_render() {
-    php /phpdoc/phd/render.php \
+    echo -e "${GREEN}Rendering documentation ${NC}"
+    errmsg=`php /phpdoc/phd/render.php \
     --docbook /phpdoc/doc-base/.manual.xml \
     --package PHP \
     --format php \
-    --output /phpdoc/
+    --output /phpdoc/ 2>&1 > /dev/null`
+
+    if [ ! -z "$errmsg" ]; then
+        echo -e "${RED}Rendering failed. Error: ${NC}"
+        echo -e "$errmsg"
+        return 1
+    else
+        echo -e "${GREEN}Rendering complete ${NC}"
+        return 0
+    fi
+    
 }
-#docs_configure
-#docs_render
 
 # Symlink built docs into website
 rm -rf /phpdoc/web-php/manual/en && ln -s /phpdoc/php-web /phpdoc/web-php/manual/en
@@ -29,10 +54,22 @@ rm -rf /phpdoc/web-php/manual/en && ln -s /phpdoc/php-web /phpdoc/web-php/manual
 # Serve PHP site using built-in webserver
 php -S 0.0.0.0:8080 -t /phpdoc/web-php /phpdoc/web-php/.custom-router.php > /dev/null 2>&1 &
 
-# Provide user menu for next action to run
+# Define the user menu
+menu="${BLUE}
+
+Press a key to perform an action:
+- (b)uild         - Run both configure and render steps.
+- (c)onfigure     - Validates and prepares the documentation XML.
+- (r)ender        - Render the docs to PHP for display.
+- (q)uit          - Quit this builder application.
+${NC}
+"
+
+# Show the user menu between jobs
 while true
 do
-    read -n 1 -s -r -p $'Press key to (c)onfigure, (r)ender, (b)uild or (q)uit:\n' key
+    echo -e "$menu"
+    read -n 1 -s -r -p $"" key
     if [ "$key" == 'c' ]; then
         docs_configure
     elif [ "$key" == 'r' ]; then
@@ -40,7 +77,6 @@ do
     elif [ "$key" == 'q' ]; then
         exit 0
     elif [ "$key" == 'b' ]; then
-        docs_configure
-        docs_render
+        docs_configure && docs_render
     fi
 done
